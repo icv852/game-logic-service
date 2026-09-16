47c40bafd248162599cc6256cedf8729d2af832e
Victor Cheng <victorcheng@Victors-MacBook-Air.local>
2026-09-16 21:39:30 +0800

    handle defect

--- src/utils/effect-runners.ts
+++ src/utils/effect-runners.ts
@@ -1,4 +1,4 @@
-import { Effect, pipe } from "effect";
+import { Cause, Effect, Option, pipe } from "effect";
 import type { Context } from "koa";
 import HttpStatusCode from "../constants/http-status-code.js";
 import { CustomError } from "./errors.js";
@@ -17,19 +17,31 @@ export const handleHttpRequest = async (
         Effect.succeed(requestBody),
         Effect.flatMap(validateByZodSchema(schema)),
         Effect.flatMap(handler),
-        Effect.match({
+        Effect.matchCause({
             onSuccess: (data) => {
                 ctx.status = HttpStatusCode.OK
                 ctx.body = encode ? encode(data) : { ...data }
                 logger.info(`${ctx.status} ${ctx.request.method} ${ctx.request.path}. ${JSON.stringify(ctx.body)}`)
             },
-            onFailure: (e: unknown) => {
+            onFailure: (cause) => {
+                const failure = Cause.findErrorOption(cause)
+
+                // handle defect
+                if (Option.isNone(failure)) {
+                    ctx.status = HttpStatusCode.INTERNAL_SERVER_ERROR
+                    ctx.body = { error: { error_code: "INTERNAL_SERVER_ERROR", message: "Internal server error." } }
+                    logger.error(`${ctx.status} ${ctx.request.method} ${ctx.request.path}. DEFECT:\n${Cause.pretty(cause)}`)
+                    return
+                }
+
+                const e = failure.value
                 if (e instanceof CustomError) {
                     ctx.status = e.statusCode
                     ctx.body = { error: { error_code: e.errorCode, message: e.message } }
                     logger.error(`${ctx.status} ${ctx.request.method} ${ctx.request.path}. ${e.errorCode}: ${e.message}`)
                     return
                 }
+
                 const message = e instanceof Error ? e.message : String(e)
                 ctx.status = HttpStatusCode.INTERNAL_SERVER_ERROR
                 ctx.body = { error: { error_code: "INTERNAL_SERVER_ERROR", message } }
